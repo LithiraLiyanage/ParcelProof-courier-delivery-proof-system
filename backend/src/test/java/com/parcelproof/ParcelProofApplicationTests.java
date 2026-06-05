589e8d223709772dc930910e98685cbfc995fe26
Lithira Liyanage <lithiraliyanage666@gmail.com>
2026-06-05 17:58:56 +0530

    test: add Spring Boot context test

---
+++ backend/src/test/java/com/parcelproof/ParcelProofApplicationTests.java
@@ -0,0 +1,3 @@
+package com.parcelproof;
+import org.junit.jupiter.api.Test;import org.springframework.boot.test.context.SpringBootTest;
+@SpringBootTest class ParcelProofApplicationTests{@Test void contextLoads(){}}
